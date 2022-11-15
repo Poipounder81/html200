0f492d6c4e76c68f3e46579a16391c1702af9d63
Poipounder81 <103324222+Poipounder81@users.noreply.github.com>
2022-11-14 19:09:58 -0800

    countdown file lesson02 assignment 2

---
+++ lesson02/countdown.js
@@ -0,0 +1,13 @@
+const a = 'Welcome To ...';
+console.log (a);
+
+let wordOne = 'Lesson02';
+let wordTwo = 'Countdown';
+console.log (wordOne + ' ' + wordTwo);
+
+let z = 10;
+
+while (z > -1) {
+  console.log(z);
+  z--;
+}
