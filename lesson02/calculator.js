fe58861bdf1e1ee46d3ea5467bb009f16bbc2a86
Poipounder81 <103324222+Poipounder81@users.noreply.github.com>
2022-11-18 12:55:57 -0800

    updating html200 (master)

---
+++ lesson02/calculator.js
@@ -0,0 +1,56 @@
+const a = 'Welcome To ...';
+console.log (a);
+
+let wordOne = 'Lesson02';
+let wordTwo = 'Calculator';
+console.log (wordOne + ' ' + wordTwo);
+
+let wordThree = 'Addition';
+let wordFour = 'Example:';
+console.log (wordThree + ' ' + wordFour);
+var b = 2;
+console.log(b + 2);
+
+let wordFive = 'Substraction';
+console.log (wordFive + ' ' + wordFour);
+var c = 3;
+console.log(c - 3);
+
+let wordSix = 'Multiplication';
+console.log (wordSix + ' ' + wordFour);
+var d = 4;
+console.log(d * 4);
+
+let wordSeven = 'Division';
+console.log (wordSeven + ' ' + wordFour);
+var e = 5;
+console.log(e / 5);
+
+const bcde = '(b + c) * (d - e) =';
+console.log (bcde);
+console.log((b + c) * (d - e));
+const bdce = '(b + d) / (c - e) =';
+console.log (bcde);
+console.log((b + d) / (c - e));
+
+let wordEight = 'Modulo';
+console.log (wordEight + ' ' + wordFour);
+var f = 6;
+console.log(f % 6);
+
+let wordNine = 'Exponentiation';
+console.log (wordNine + ' ' + wordFour);
+var g = 7;
+console.log(g ** 7);
+
+let wordTen = 'Increment';
+console.log (wordTen + ' ' + wordFour);
+let h = 8;
+console.log(h++);
+console.log(h);
+
+let wordEleven = 'Decrement';
+console.log (wordTen + ' ' + wordFour);
+let i = 9;
+console.log(i--);
+console.log(i);
