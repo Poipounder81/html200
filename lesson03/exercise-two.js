49392fad446aabb102574260df644e90e2d0dc85
Poipounder81 <103324222+Poipounder81@users.noreply.github.com>
2022-12-03 12:53:08 -0800

    lesson3 assignment2

---
+++ lesson03/exercise-two.js
@@ -0,0 +1,7 @@
+//Make a string variable of a sentence with least five words and a period.
+const jurassicPark = 'life will find a way';
+console.log(jurassicPark);
+
+///Make an array from that sentence and console.log the third word.
+const usingSplit = jurassicPark.split(' ');
+console.log(usingSplit[2]);
